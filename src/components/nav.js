3c5bd540a58507fed3d45bffcf15bdfa5e708e84
nysteo <teonys@nyu.edu>
2020-10-16 17:47:54 -0400

    basic navbar

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,10 +1,33 @@
 import React from 'react'
+import { Link } from 'gatsby';
+import PropTypes from 'prop-types';
+import styled, {css} from 'styled-components';
+import {navLinks } from '../config.js'
+
+const StyledHeader = styled.header`
+
+`;
+
+const StyledNav = styled.nav`
+
+`;
+
+
+const StyledLinks = styled.div`
+
+`;
 
 const nav = () => {
     return (
-        <div>
-            
-        </div>
+        <StyledHeader>
+            <StyledNav>
+                <StyledLinks>
+                    {navLinks.map(({url, name}, index) => (
+                        <li><Link>{index + 1}.{name}</Link></li>
+                    ))}
+                </StyledLinks>
+            </StyledNav> 
+        </StyledHeader>
     )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Hero from '../components/sections/hero.js'
+import Nav from '../components/nav.js'
 
 export default function Home() {
   return (
     
     <div>
+      <Nav />
       <Hero />
     </div>
   )
