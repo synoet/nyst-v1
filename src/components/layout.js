0d0aabc009c140a338e5a726eb423f3b6de06506
nysteo <teonys@nyu.edu>
2020-10-16 16:13:02 -0400

    global styles and fonts setup

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,9 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import PropTypes from 'prop-types';
+import styled, { ThemeProvider } from 'styled-components';
+import Nav from 'nav.js';
+import Side from 'side.js';
+
 
 const layout = () => {
     return (
---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,7 @@
+import {createGlobalStyle} from 'styled-components';
+import Fonts from './fonts';
+
+
+const GlobalStyle = createGlobalStyle`
+    ${fonts}
+    `;
