a259ce1eefc27966a0ba62f63e88369189ab3338
nysteo <teonys@nyu.edu>
2020-10-16 17:24:37 -0400

    config.js

---
+++ src/config.js
@@ -0,0 +1,41 @@
+module.exports = {
+    email: 'teonys@nyu.edu',
+
+    socialMedia: [
+        {
+            name: 'GitHub',
+            url: 'https://github.com/nysteo'
+        },
+        {
+            name: 'LinkedIn',
+            url: 'https://www.linkedin.com/in/nysteo',
+        },
+        {
+            name: 'Twitter',
+            url: 'https://twitter.com/nysteo'
+        }
+    ],
+    navLinks: [
+        {
+            name: 'About',
+            url: '/#about',
+        },
+        {
+            name: 'Experience',
+            url: '/#jobs',
+        },
+        {
+            name: 'Work',
+            url: '/#projects'
+        },
+        {
+            name: 'Blog',
+            url: '/#blog'
+        },
+        {
+            name: 'Contact',
+            url: '/#contact'
+        }
+        
+    ]
+}
