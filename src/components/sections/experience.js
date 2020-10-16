9aacce533c0bcda8061d2940e28c7d6d850680b6
nysteo <teonys@nyu.edu>
2020-10-16 17:26:19 -0400

    sections

---
+++ src/components/sections/experience.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const experience = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default experience
---
+++ src/components/sections/projects.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const projects = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default projects
