5e01c7f659eed57cb2ab0d5d3d6e7a7f9bd6b4ae
nysteo <teonys@nyu.edu>
2020-10-21 23:14:21 -0400

    fixed loading speed

--- src/components/loader.js
+++ src/components/loader.js
@@ -31,7 +31,7 @@ const Loader = ({finishLoading}) => {
     const timeout = setTimeout(() => setIsMounted(true), 5);
     setTimeout(() => {
         finishLoading()
-    }, 3000)
+    }, 2500)
     return () => clearTimeout(timeout);
   }, []);
 
